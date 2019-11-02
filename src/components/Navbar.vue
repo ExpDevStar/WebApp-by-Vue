88641388690b2b81a1749715d0c9ae8df58b2422
ExpDevStar <expdevstar000@outlook.com>
2019-11-02 22:08:52 +0100

    Changes to login view and layout of register view.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,87 +1,130 @@
 <template>
-  <b-navbar wrapper-class="container" transparent spaced :shadow="!homepage">
-    <template slot="brand">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }"
-        >karaoke-app</b-navbar-item
-      >
-    </template>
-
-    <template slot="start">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">Home</b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{ path: '/about' }"
-        >About</b-navbar-item
-      >
-    </template>
-
-    <template slot="end">
-      <b-navbar-item tag="div">
-        <div class="buttons">
-          <button
-            class="button is-primary"
-            @click="isRegisterModalActive = true"
-            :class="{ 'is-inverted': homepage }"
-            >Sign up</button
-          >
-          <button
-            class="button is-primary is-outlined"
-            @click="isLoginModalActive = true"
-            :class="{ 'is-inverted': homepage }"
-          >
-            Login
-          </button>
-        </div>
-      </b-navbar-item>
-      <!--login modal-->
-      <b-modal :active.sync="isLoginModalActive">
-        <div class="container form">
-          <form action="" class="box">
-            <div class="buttons">
-              <b-button expanded class="is-facebook">
-                <span class="icon">
-                  <i class="fab fa-facebook"></i>
-                </span>
-                <span>Login with Facebook</span>
-              </b-button>
-              <b-button expanded class="is-google">
-                <span class="icon">
-                  <i class="fab fa-google"></i>
-                </span>
-                <span>Login with Google</span>
-              </b-button>
-            </div>
-            <div class="separator">OR</div>
-            <b-field label="Email">
-              <b-input placeholder="Email"></b-input>
-            </b-field>
-            <b-field label="Password">
-              <b-input
-                type="password"
-                placeholder="Password"
-                minlength="6"
-                validation-message="Password must contain at least 6 characters!"
-              >
-              </b-input>
-            </b-field>
-            <div class="buttons">
-              <b-button expanded type="is-success">Login</b-button>
-              <span
-                >You don't have an account?
-                <a @click="changeAuthModalView">sign in!</a></span
-              >
-            </div>
-          </form>
-        </div>
-      </b-modal>
-      <!--register modal-->
-      <b-modal :active.sync="isRegisterModalActive">
-        wip
-      </b-modal>
-    </template>
-  </b-navbar>
+  <nav>
+    <b-navbar wrapper-class="container" transparent spaced :shadow="!homepage">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }"
+          >karaoke-app</b-navbar-item
+        >
+      </template>
+      <template slot="start">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }"
+          >Home</b-navbar-item
+        >
+        <b-navbar-item tag="router-link" :to="{ path: '/about' }"
+          >About</b-navbar-item
+        >
+      </template>
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <button
+              class="button is-primary"
+              @click="activateRegisterModal()"
+              :class="{ 'is-inverted': homepage }"
+            >
+              Sign up
+            </button>
+            <button
+              class="button is-primary is-outlined"
+              @click="activateLoginModal()"
+              :class="{ 'is-inverted': homepage }"
+            >
+              Login
+            </button>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+    <!--
+    login modal
+    -->
+    <b-modal :active.sync="isLoginModalActive">
+      <div class="container form">
+        <form action="" class="box">
+          <header class="card-header-title">
+            Login to Karaoke-app!
+          </header>
+          <div class="buttons">
+            <b-button expanded class="is-facebook">
+              <span class="icon">
+                <i class="fab fa-facebook"></i>
+              </span>
+              <span>Login with Facebook</span>
+            </b-button>
+            <b-button expanded class="is-google">
+              <span class="icon">
+                <i class="fab fa-google"></i>
+              </span>
+              <span>Login with Google</span>
+            </b-button>
+          </div>
+          <div class="separator">OR</div>
+          <b-field label="Email">
+            <b-input placeholder="Email"></b-input>
+          </b-field>
+          <b-field label="Password">
+            <b-input type="password" placeholder="Password"> </b-input>
+          </b-field>
+          <div class="buttons">
+            <b-button expanded type="is-success">Login</b-button>
+            <span
+              >You don't have an account?
+              <a @click="changeAuthModalView">Sign up!</a></span
+            >
+          </div>
+        </form>
+      </div>
+    </b-modal>
+    <!--
+    register modal
+    -->
+    <b-modal :active.sync="isRegisterModalActive">
+      <div class="container form">
+        <form action="" class="box" style="width:412.31px">
+          <header class="card-header-title">
+            Sign up to Karaoke-app!
+          </header>
+          <b-field label="Username">
+            <b-input placeholder="Username" minlength="3"></b-input>
+          </b-field>
+          <b-field label="Email">
+            <b-input placeholder="Email"></b-input>
+          </b-field>
+          <b-field label="Password">
+            <b-input
+              placeholder="At least 6 chars."
+              type="password"
+              minlength="6"
+              validation-message="Password must contain at least 6 characters!"
+            ></b-input>
+          </b-field>
+          <b-field label="Confirm Password">
+            <b-input
+              placeholder="Confirm Password"
+              type="password"
+              minlength="6"
+              validation-message="Password must contain at least 6 characters!"
+            ></b-input>
+          </b-field>
+          <b-checkbox>
+            I accept the Terms & Conditions
+          </b-checkbox>
+          <b-button expanded class="is-success">
+            <span>Register</span>
+          </b-button>
+          <span
+            >Already have an account?
+            <a @click="changeAuthModalView">Login!</a>
+          </span>
+        </form>
+      </div>
+    </b-modal>
+  </nav>
 </template>
 
 <script>
 export default {
+  components: {},
   props: ["homepage"],
   data() {
     return {
@@ -93,6 +136,12 @@ export default {
     changeAuthModalView() {
       this.isLoginModalActive = !this.isLoginModalActive;
       this.isRegisterModalActive = !this.isRegisterModalActive;
+    },
+    activateLoginModal() {
+      this.isLoginModalActive = true;
+    },
+    activateRegisterModal() {
+      this.isRegisterModalActive = true;
     }
   }
 };
