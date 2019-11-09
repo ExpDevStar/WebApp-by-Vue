9ba8a88fcef98632d0a8096b7310f05d15bcd6fe
ExpDevStar <expdevstar000@outlook.com>
2019-11-09 21:58:04 +0100

    add simple song header with report modal

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -5,13 +5,66 @@
         <SongPlayer provider="youtube" embedId="FTQbiNvZqaY" />
       </div>
     </section>
+    <header class="section">
+      <div class="container">
+        <div class="level">
+          <div class="level-left">
+            <div class="level-item">
+              <h4 class="title is-4">
+                {{ songMeta.artist }} - {{ songMeta.title }}
+              </h4>
+            </div>
+
+            <div class="level-item">
+              <b-rate icon-pack="fas" spaced size="is-small"></b-rate>
+            </div>
+          </div>
+
+          <div class="level-right">
+            <div class="level-item">updated {{ updatedAt }}</div>
+
+            <div class="level-item">
+              <b-tooltip label="Report" type="is-dark"
+                ><b-button type="is-text" @click="reportModal"
+                  ><i class="fas fa-exclamation-triangle"></i></b-button
+              ></b-tooltip>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   </main>
 </template>
 
 <script>
 import SongPlayer from "@/components/SongPlayer.vue";
+import ReportModal from "@/components/ReportModal.vue";
+import moment from "moment";
 
 export default {
-  components: { SongPlayer }
+  components: { SongPlayer },
+  data() {
+    return {
+      songMeta: {
+        title: "Africa",
+        artist: "Toto",
+        updatedAt: Date.now()
+      }
+    };
+  },
+  computed: {
+    updatedAt() {
+      return moment(this.songMeta.updatedAt).fromNow();
+    }
+  },
+  methods: {
+    reportModal() {
+      this.$buefy.modal.open({
+        parent: this.$root,
+        component: ReportModal,
+        hasModalCard: true
+      });
+    }
+  }
 };
 </script>
