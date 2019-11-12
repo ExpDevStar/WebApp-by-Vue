365f0b3db36fe68cb2e63697a0476f190d14d677
ExpDevStar <expdevstar000@outlook.com>
2019-11-12 18:48:14 +0100

    Deleted redundant home link in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,7 +12,6 @@
       >
     </template>
     <template slot="start">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">Home</b-navbar-item>
       <b-navbar-item tag="router-link" :to="{ path: '/about' }"
         >About</b-navbar-item
       >
